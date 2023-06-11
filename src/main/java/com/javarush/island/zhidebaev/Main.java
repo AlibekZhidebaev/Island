268468a09d5620ca2278eb3c70fa871714e3b37b
AlibekZhidebaev <foxmal81@gmail.com>
2023-06-11 20:05:19 +0600

    Initialisation without .idea/

--- src/main/java/com/javarush/island/zhidebaev/Main.java
+++ src/main/java/com/javarush/island/zhidebaev/Main.java
@@ -1,6 +1,4 @@
-package com.javarush.island.zhidebaev;
-
-public class Main {
+package com.javarush.island.zhidebaev;public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
